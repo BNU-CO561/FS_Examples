2bfbf48d7fb7495af6196b2d5ac7203e8d22ad2d
Kate A Ibom <72076413+KateTurton@users.noreply.github.com>
2024-02-25 12:42:01 +0000

    Added modules and examples

---
+++ FS_Examples/Examples.fs
@@ -0,0 +1,8 @@
+﻿module Examples
+module Example1 =
+    let run() =
+        printfn "Example 1"
+
+module Example2 =
+    let run() =
+        printfn "Example 2"
--- FS_Examples/Program.fs
+++ FS_Examples/Program.fs
@@ -1,2 +1,5 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿module Program
+
+// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+let example = Examples.Example2.run()
