18e5bf888487d641cab5f696ea8b6027ccddcdbc
Kate A Ibom <72076413+KateTurton@users.noreply.github.com>
2024-02-28 19:34:41 +0000

    Started example 1

--- FS_Examples/Examples.fs
+++ FS_Examples/Examples.fs
@@ -1,8 +1,19 @@
 ﻿module Examples
+
+// Define a module that will ask the user to enter their name, height and age. The module will then print the name, height and age out to the console.
 module Example1 =
     let run() =
         printfn "Example 1"
 
+
 module Example2 =
     let run() =
         printfn "Example 2"
+
+module Example3 =
+    let run() =
+        printfn "Example 3"
+
+module Example4 =
+    let run() =
+        printfn "Example 4"
